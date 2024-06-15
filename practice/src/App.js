74c1ae7136ad11200f150303a52d31ff6413e2b9
Nathaniel <ubercube4@gmail.com>
2024-06-14 23:55:29 -0400

    Built routing

---
+++ practice/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Lists from './pages/Lists'
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/lists" element={<Lists />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ practice/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+
+
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/lists">Lists</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar
